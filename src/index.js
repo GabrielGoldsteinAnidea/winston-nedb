9c3ed6f032ca76586ffb4c6ea75694edf852062d
Kothique Félixx <kothique@protonmail.ch>
2019-02-01 21:30:58 +0300

    Rewrite in ES6

--- src/index.js
+++ src/index.js
@@ -1,128 +1,138 @@
-// var util = require('util');
-// var os = require('os');
-// var Stream = require('stream').Stream;
-//
-// var Datastore = require('nedb');
-// var winston = require('winston');
-//
-// /**
-//   * @param {object?}  options
-//   * @param {string?}  options.filename - If none is given, db will not be persisted.
-//   * @param {boolean?} options.compact - Default: false. If true, every removal of logs will launch a database compaction.
-//   *                                     Keep in mind that compaction takes a bit of time and is synchronous.
-//   * @param {boolean?} options.timestamp - Default: true. If false, db will not store timestamps on logs.
-//  */
-// var Nedb = exports.Nedb = function Nedb(options = {}) {
-//   var filename = options.filename || null;
-//   var compact = typeof options.compact === 'boolean' ? options.compact : false;
-//   var timestamp = typeof options.timestamp === 'boolean' ? options.timestamp : true;
-//
-// 	var self = this;
-// 	self.name = 'nedb';
-// 	self.level = options.level || 'info';
-//
-// 	if (filename) {
-// 		self.db = new Datastore({ filename: filename, autoload: true });
-// 	} else {
-// 		self.db = new Datastore();
-// 	};
-//
-//   self.timestamp = timestamp;
-// 	if (timestamp) {
-// 		self.db.ensureIndex({ fieldName: 'timestamp' });
-//   }
-//
-//   self.compact = compact;
-// }
-// util.inherits(Nedb, winston.Transport);
-//
-// /**
-//  * Remove all logs older than timestamp.
-//  * This function is asynchronous except if compact was set to true in options.
-//  * @param {number}                    timestamp - In ms since current time.
-//  * @param {(err, numRemoved) => void} callback
-//  */
-// Nedb.prototype.rotate = function rotate(timestamp, callback) {
-//   var self = this;
-//
-//   var currentTime = new Date().getTime();
-//   var minTime = currentTime - timestamp;
-//   self.db.remove({ timestamp: { $lt: minTime } }, callback);
-//   self.db.persistence.compactDatafile; // We can do this because compact will be queued by nedb.
-// }
-//
-// /**
-//  * Inherit from winston
-// */
-//
-// /**
-//  * @param {object} info
-//  * @param {string} message
-//  * @param {any} meta - Must be serializable.
-//  * @param {(err, newDoc) => void} - Callback.
-//  */
-// Nedb.prototype.log = function log(level, msg, meta, callback) {
-// 	var self = this;
-//
-// 	var entry = {
-// 		level: level,
-// 		msg: msg,
-// 	};
-//
-//   if (this.timestamp) {
-//     entry.timestamp = new Date().getTime();
-//   }
-//
-// 	/* If provided, metadata is added, nested in .meta property. */
-// 	if (meta) {
-//     entry.meta = meta;
-//  	}
-//
-//   // Store it in db.
-//   this.db.insert(entry, callback);
-// }
-//
-// /**
-//  * @param {object?}             options
-//  * @param {Date?}               options.from
-//  * @param {Date?}               options.until
-//  * @param {number?}             options.rows
-//  * @param {number?}             options.start
-//  * @param {('asc' | 'desc')?}   options.order
-//  * @param {string[]?}           options.fields
-//  * @param {(err, rows) => void} callback
-//  */
-// Nedb.prototype.query = function (options, callback) {
-// 	if (typeof options === 'function') {
-//   	callback = options;
-//   	options = {};
-// 	}
-//
-// 	var count = 0;
-// 	var query = { $where : function () {
-// 		if (options.from && this.timestamp && this.timestamp <= options.from) {
-// 			return false;
-// 		} else if (options.until && this.timestamp && this.timestamp >= options.until) {
-// 			return false;
-// 		} else {
-// 			return true;
-// 		}
-// 	}};
-//
-// 	var cursor = this.db.find(query);
-// 	if (options.order === 'asc') {
-// 		cursor.sort({ timestamp: 1, msg: 1 });
-// 	} else {
-// 		cursor.sort({ timestamp: -1, msg: -1 });
-// 	}
-//
-// 	if (options.start) {
-// 		cursor.skip(options.start);
-// 	}
-//
-// 	if (options.rows) {
-// 		cursor.limit(options.rows);
-// 	}
-//
-// 	cursor.exec(callback);
-// }
+import 'babel-polyfill';
+import Transport from 'winston-transport';
+import Datastore from 'nedb';
+
+export default class NeDB extends Transport {
+  /**
+    * @param {object?}  options
+    * @param {string?}  options.filename - If none is given, db will not be persisted.
+    * @param {boolean?} options.compact - Default: false. If true, every removal of logs will launch a database compaction.
+    *                                     Keep in mind that compaction takes a bit of time and is synchronous.
+   */
+  constructor(options = {}) {
+    super(options);
+
+    const filename = options.filename || null;
+    this._compact = typeof options.compact === 'boolean' ? options.compact : false;
+
+    this.name = 'nedb';
+
+    this._db = filename
+      ? new Datastore({ filename, autoload: true })
+      : new Datastore;
+
+    this._db.ensureIndex({ fieldName: 'timestamp' });
+  }
+
+  /**
+   * Remove all logs older than timestamp.
+   * This function is asynchronous except if compact was set to true in options.
+   * @param {number} timestamp - In ms since current time.
+   * @return {Promise<number>} - Number of rows removed.
+   * @throws - Errors from Datastore#remove().
+   */
+  async rotate(timestamp) {
+    const currentTime = new Date().getTime();
+    const minTime = currentTime - timestamp;
+
+    return new Promise((resolve, reject) => this._db.remove(
+      { timestamp: { $lt: minTime } },
+      (err, numRemoved) => {
+        if (err) { return reject(err); }
+
+        if (this._compact) {
+          this._db.persistence.compactDatafile();
+        }
+        resolve(numRemoved);
+      }
+    ));
+  }
+
+  /**
+   * @param {object?}             options
+   * @param {Date?}               options.from
+   * @param {Date?}               options.until
+   * @param {number?}             options.rows
+   * @param {number?}             options.start
+   * @param {('asc' | 'desc')?}   options.order
+   * @param {string[]?}           options.fields
+   * @param {(err, res) => void}  callback
+   */
+  async query(options, callback) {
+    options = normalizeQuery(options);
+
+    const count = 0;
+    const query = { $where: function () {
+      if (options.from && this.timestamp <= options.from) {
+        return false;
+      } else if (options.until && this.timestamp >= options.until) {
+        return false;
+      } else {
+        return true;
+      }
+    }};
+
+    const cursor = this._db.find(query);
+    if (options.order === 'asc') {
+      cursor.sort({ timestamp: 1, msg: 1 });
+    } else {
+      cursor.sort({ timestamp: -1, msg: -1 });
+    }
+
+    if (options.start) {
+      cursor.skip(options.start);
+    }
+
+    if (options.rows) {
+      cursor.limit(options.rows);
+    }
+
+    cursor.exec((err, rows) => err ? callback(err) : callback(undefined, rows));
+  }
+
+  log(info, callback) {
+    const { level, message, ...meta } = info;
+
+    setImmediate(() => this.emit('logged', info));
+
+    this._db.insert(
+      {
+        level,
+        message,
+        ...meta && { meta }
+      },
+      (err, row) => err ? callback(err) : callback()
+    );
+  }
+};
+
+/* From winston/transports/file.js */
+function normalizeQuery(options) {
+  options = options || {};
+
+  // limit
+  options.rows = options.rows || options.limit || 10;
+
+  // starting row offset
+  options.start = options.start || 0;
+
+  // now
+  options.until = options.until || new Date();
+  if (typeof options.until !== 'object') {
+    options.until = new Date(options.until);
+  }
+
+  // now - 24
+  options.from = options.from || (options.until - (24 * 60 * 60 * 1000));
+  if (typeof options.from !== 'object') {
+    options.from = new Date(options.from);
+  }
+
+  // 'asc' or 'desc'
+  options.order = options.order || 'desc';
+
+  // which fields to select
+  options.fields = options.fields;
+
+  return options;
+}
